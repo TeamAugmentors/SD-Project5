844e29afef06f7ea7491f4a658b1ea48ba31819f
AtiqurRahmanAni <56642339+AtiqurRahmanAni@users.noreply.github.com>
2022-01-24 00:56:56 +0600

    My past commit was bad. Git command line is the way to go

---
+++ JobSeekerWeb/CustomUtils/CustomSession.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JobSeekerWeb.CustomUtils
+{
+    public static class CustomSession
+    {
+
+        public static SessionObject session = new SessionObject();
+
+        public static SessionObject GetSession()
+        {
+            return session; 
+        }
+    }
+}
---
+++ JobSeekerWeb/CustomUtils/SessionObject.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JobSeekerWeb.CustomUtils
+{
+    public class SessionObject
+    {
+        private Dictionary<string, string> Map = new Dictionary<string, string>();
+
+        public void set(string key, string value)
+        {
+            Map[key] = value;   
+        }
+
+        public string get(string key)
+        {
+            return Map[key];
+        }
+
+        public void clear()
+        {
+            Map.Clear();    
+        }
+    }
+}
