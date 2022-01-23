27f3eaa97c94e306d2e663d4fa3484d4f188d90c
AtiqurRahmanAni <56642339+AtiqurRahmanAni@users.noreply.github.com>
2022-01-24 00:47:01 +0600

    Signin and sign up addes

--- JobSeekerWeb/Controllers/DashboardController.cs
+++ JobSeekerWeb/Controllers/DashboardController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using JobSeekerWeb.Models;
+using JobSeekerWeb.CustomUtils;
 
 namespace JobSeeker.Controllers
 {
@@ -11,7 +13,17 @@ namespace JobSeeker.Controllers
         // GET: Dashboard
         public ActionResult Overview()
         {
-            return View();
+
+            //string name = Session["name"].ToString();
+            //string user_name = Session["user_name"].ToString();
+            //string mail = Session["mail"].ToString();
+            //string phone_no = Session["phone_no"].ToString();
+            //string billind_info = Session["billind_info"].ToString();
+            //string picture = Session["picture"].ToString();
+
+            CustomSession.GetSession().set("id", "10");
+
+            return View((Object)CustomSession.GetSession());
         }
     }
 }
--- JobSeekerWeb/Controllers/UserController.cs
+++ JobSeekerWeb/Controllers/UserController.cs
@@ -3,19 +3,73 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using JobSeekerWeb.Models;
+using JobSeekerWeb.CustomUtils;
 
 namespace JobSeeker.Controllers
 {
     public class UserController : Controller
     {
+        jobseekerWebEntities db = new jobseekerWebEntities();
         // GET: User
+        [HttpPost]
+        public ActionResult SignIn(user user)
+        {
+            string query = "SELECT * FROM users";
+            List<user> u = db.users.SqlQuery(query).ToList();
+            foreach(var x in u)
+            {
+                if(user.mail == x.mail && user.password == x.password)
+                {
+
+      
+                    //Session["id"] = x.id;
+                    //Session["name"] = x.name;
+                    //Session["mail"] = x.mail;
+                    //Session["user_name"] = x.user_name;
+                    //Session["phone_no"] = x.phone_no;
+                    //Session["billind_info"] = x.billing_info;
+                    //Session["picture"] = x.picture;
+
+
+                    Response.Redirect("/Dashboard/Overview");
+                }
+            }
+            return View();
+        }
         public ActionResult SignIn()
         {
             return View();
         }
-        public ActionResult SignUp()
+
+        [HttpPost]
+        public ActionResult SignUp(user user)
         {
+            //string query = $"INSERT INTO user (name, user_name, mail, password) VALUES ('{@user.name}', '{@user.user_name}', '{@user.mail}', '{@user.password}')";
+            if(ModelState.IsValid)
+            {
+                db.users.Add(user);
+                db.SaveChanges();
+
+                string query = $"SELECT id FROM users where mail = '{user.mail}'";
+                
+                var id = db.users.Where(temp => temp.mail == user.mail).Select(temp => temp.id).SingleOrDefault();
+                
+                string query1 = $"INSERT INTO freelancer(id) values({id})";
+                string query2 = $"INSERT INTO hirer(id) values({id})";
+                
+                db.Database.ExecuteSqlCommand(query1);
+                db.Database.ExecuteSqlCommand(query2);
+
+                Response.Redirect("SignIn");
+            }
             return View();
         }
+        public ActionResult SignUp()
+        {
+
+            return View();
+        }
+
     }
 }
